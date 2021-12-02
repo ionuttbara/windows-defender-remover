6ee675c35c0e2253705af018864250ba5a33084c
John Bara <76656855+jbara2002@users.noreply.github.com>
2021-12-02 14:24:14 +0200

    CODE Source for n-EAS Defender Remover

---
+++ delete_def.bat
@@ -0,0 +1,133 @@
+pushd "%CD%"
+CD /D "%~dp0"
+echo off 
+
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Security and Maintenance" /f
+::start using Fidelity to remove packages
+
+fidelity /o /c Microsoft-Windows-SecurityCenter /r
+fidelity /o /c Windows-Defender /r
+fidelity /o /c Microsoft-Windows-HVSI /r
+fidelity /o /c Microsoft-Windows-SecureStartup /r
+fidelity /o /c Microsoft-Windows-Killbits /r
+fidelity /o /c Microsoft-Windows-SenseClient /r
+
+:: start using Fidelity for removing services
+
+
+sc delete SgrmBroker
+sc delete SgrmAgent
+sc delete SecurityHealthService
+sc delete WdBoot
+sc delete WdFiltrer
+sc delete WdNisSvc
+sc delete WdNisDrv
+sc delete wscsvc
+sc delete Sense
+sc delete WinDefend
+
+::import some registry
+
+
+PowerRun.exe Regedit.exe /S def.reg
+Regedit /S def.reg
+
+::tweaking boot, disable elam and VBS with LSA
+bcdedit /set {current} disableelamdrivers yes
+bcdedit /set nointegritychecks on
+bcdedit /set hypervisorlaunchtype off
+bcdedit /set useplatformclock No
+bcdedit /set {current} bootmenupolicy Legacy
+bcdedit /set tpmbootentropy ForceDisable
+bcdedit /create {0cb3b571-2f2e-4343-a879-d86a476d7215} /d
+bcdedit /set {bootmgr} bootsequence {0cb3b571-2f2e-4343-a879-d86a476d7215}
+bcdedit /set {0cb3b571-2f2e-4343-a879-d86a476d7215} loadoptions DISABLE-LSA-ISO,,DISABLE-VBS
+bcdedit /set {current} bootstatuspolicy ignoreallfailures
+
+
+::removing Crusters
+
+takeown /f C:\Windows\System32\SecurityHealthSystray.exe
+cacls "C:\Windows\System32\SecurityHealthSystray.exe" /E /P %username%:F
+del /F /Q "C:\Windows\System32\SecurityHealthSystray.exe"
+takeown /f C:\Windows\System32\SecurityHealthService.exe
+cacls "C:\Windows\System32\SecurityHealthService.exe" /E /P %username%:F
+del /F /Q "C:\Windows\System32\SecurityHealthService.exe"
+takeown /f C:\Windows\System32\SecurityHealthHost.exe
+cacls "C:\Windows\System32\SecurityHealthHost.exe" /E /P %username%:F
+del /F /Q "C:\Windows\System32\SecurityHealthHost.exe"
+takeown /f C:\Windows\System32\drivers\Ndu.sys
+cacls "C:\Windows\System32\drivers\Ndu.sys" /E /P %username%:F
+del /F /Q "C:\Windows\System32\drivers\Ndu.sys"
+takeown /f C:\Windows\System32\drivers\SgrmAgent.sys
+cacls "C:\Windows\System32\drivers\SgrmAgent.sys" /E /P %username%:F
+del /F /Q "C:\Windows\System32\drivers\SgrmAgent.sys"
+takeown /f C:\Windows\System32\drivers\WdDevFlt.sys
+cacls "C:\Windows\System32\drivers\WdDevFlt.sys" /E /P %username%:F
+del /F /Q "C:\Windows\System32\drivers\WdDevFlt.sys"
+takeown /f C:\Windows\System32\drivers\WdBoot.sys
+cacls "C:\Windows\System32\drivers\WdBoot.sys" /E /P %username%:F
+del /F /Q "C:\Windows\System32\drivers\WdBoot.sys"
+takeown /f C:\Windows\System32\drivers\WdFilter.sys
+cacls "C:\Windows\System32\drivers\WdFilter.sys" /E /P %username%:F
+del /F /Q "C:\Windows\System32\drivers\WdFilter.sys"
+takeown /f C:\Windows\System32\drivers\WdNisDrv.sys
+cacls "C:\Windows\System32\drivers\WdNisDrv.sys" /E /P %username%:F
+del /F /Q "C:\Windows\System32\drivers\WdNisDrv.sys"
+takeown /f C:\Windows\System32\drivers\wd
+cacls "C:\Windows\System32\drivers\wd" /E /P %username%:F
+rmdir /s /q "C:\Windows\System32\drivers\wd"
+takeown /f "C:\ProgramData\Microsoft\Storage Health"
+cacls "C:\ProgramData\Microsoft\Storage Health" /E /P %username%:F
+rmdir /s /q "C:\ProgramData\Microsoft\Storage Health"
+takeown /f "C:\ProgramData\Microsoft\Storage"
+cacls "C:\ProgramData\Microsoft\Storage" /E /P %username%:F
+rmdir /s /q "C:\ProgramData\Microsoft\Storage"
+takeown /f "C:\ProgramData\Microsoft\Windows Security Health"
+cacls "C:\ProgramData\Microsoft\Windows Security Health" /E /P %username%:F
+rmdir /s /q "C:\ProgramData\Microsoft\Windows Security Health"
+takeown /f "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
+cacls "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection" /E /P %username%:F
+rmdir /s /q "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
+takeown /f "C:\ProgramData\Microsoft\Windows Defender"
+cacls "C:\ProgramData\Microsoft\Windows Defender" /E /P %username%:F
+rmdir /s /q "C:\ProgramData\Microsoft\Windows Defender"
+takeown /f "C:\Windows\SystemApps\Microsoft.Windows.SecHealthUI_cw5n1h2txyewy"
+cacls "C:\Windows\SystemApps\Microsoft.Windows.SecHealthUI_cw5n1h2txyewy" /E /P %username%:F
+rmdir /s /q "C:\Windows\SystemApps\Microsoft.Windows.SecHealthUI_cw5n1h2txyewy"
+takeown /f "C:\Program Files (x86)\Windows Defender Advanced Threat Protection"
+cacls "C:\Program Files (x86)\Windows Defender Advanced Threat Protection" /E /P %username%:F
+rmdir /s /q "C:\Program Files (x86)\Windows Defender Advanced Threat Protection"
+takeown /f "C:\Program Files\Windows Defender Advanced Threat Protection"
+cacls "C:\Program Files\Windows Defender Advanced Threat Protection" /E /P %username%:F
+rmdir /s /q "C:\Program Files\Windows Defender Advanced Threat Protection"
+takeown /f "C:\Program Files (x86)\Windows Defender"
+cacls "C:\Program Files (x86)\Windows Defender" /E /P %username%:F
+rmdir /s /q "C:\Program Files (x86)\Windows Defender"
+takeown /f "C:\Program Files\Windows Defender"
+cacls "C:\Program Files\Windows Defender" /E /P %username%:F
+rmdir /s /q "C:\Program Files\Windows Defender"
+takeown /f "C:\WINDOWS\System32\drivers\wd"
+cacls "C:\WINDOWS\System32\drivers\wd" /E /P %username%:F
+rmdir /s /q "C:\WINDOWS\System32\drivers\wd"
+takeown /f "C:\ProgramData\Microsoft\Windows Defender"
+cacls "C:\ProgramData\Microsoft\Windows Defender" /E /P %username%:F
+rmdir /s /q "C:\ProgramData\Microsoft\Windows Defender"
+takeown /f "C:\Windows\System32\wscsvc.dll"
+cacls "C:\Windows\System32\wscsvc.dll" /E /P %username%:F
+del /F /Q  "C:\Windows\System32\wscsvc.dll"
+takeown /f "C:\Windows\System32\wscproxystub.dll"
+cacls "C:\Windows\System32\wscproxystub.dll" /E /P %username%:F
+del /F /Q  "C:\Windows\System32\wscproxystub.dll"
+takeown /f "C:\Windows\System32\wscisvif.dll"
+cacls "C:\Windows\System32\wscisvif.dll" /E /P %username%:F
+del /F /Q "C:\Windows\System32\wscisvif.dll"
+takeown /f "C:\Windows\System32\SecurityHealthProxyStub.dll"
+cacls "C:\Windows\System32\SecurityHealthProxyStub.dll" /E /P %username%:F
+del /F /Q "C:\Windows\System32\SecurityHealthProxyStub.dll"
+takeown /f "C:\Windows\System32\DWWIN.EXE"
+cacls "C:\Windows\System32\DWWIN.EXE" /E /P %username%:F
+del /F /Q "C:\Windows\System32\DWWIN.EXE"
+
+
+shutdown /r /f /t 0
