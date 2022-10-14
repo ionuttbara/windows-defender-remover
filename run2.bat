58c0da13e2460e7be4f2d98b3e01d068c02b6654
joni <76656855+jbara2002@users.noreply.github.com>
2022-10-14 20:13:58 +0300

    Update run2.bat

--- run2.bat
+++ run2.bat
@@ -234,6 +234,78 @@ title [80 percent] Defender Remover , version 11
 FOR /d %%f IN ("C:\Program Files\WindowsApps\Microsoft.SecHealthUI*") DO rmdir  "%%f" /s /q
 powershell "Get-AppxPackage *SecHealth* | Reset-AppxPackage"
 
+taskkill /f /im explorer.exe
+taskkill /f /im smartscreen.exe
+taskkill /f /im SecurityHealthSystray.exe
+taskkill /f /im SecurityHealthHost.exe
+taskkill /f /im SecurityHealthService.exe
+taskkill /f /im SecurityHealthHost.exe
+taskkill /f /im DWWIN.EXE
+taskkill /f /im CompatTelRunner.exe
+taskkill /f /im GameBarPresenceWriter.exe
+taskkill /f /im DeviceCensus.exe
+net stop wscsvc
+:: Removing folders
+rmdir /s /q "C:\Windows\SystemApps\Microsoft.Windows.AppRep.ChxApp_cw5n1h2txyewy"
+rmdir /s /q "C:\ProgramData\Microsoft\Windows Defender"
+rmdir /s /q "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
+rmdir /s /q "C:\ProgramData\Microsoft\Windows Security Health"
+rmdir /s /q "C:\ProgramData\Microsoft\Storage Health"
+rmdir /s /q "C:\WINDOWS\System32\drivers\wd"
+rmdir /s /q "C:\Program Files (x86)\Windows Defender"
+rmdir /s /q "C:\Program Files\Windows Defender"
+rmdir /s /q "C:\Program Files (x86)\Windows Defender Advanced Threat Protection"
+rmdir /s /q "C:\Program Files\Windows Defender Advanced Threat Protection"
+rmdir /s /q "C:\Windows\System32\SecurityHealth"
+rmdir /s /q "C:\Windows\System32\WebThreatDefSvc"
+rmdir /s /q "C:\Windows\System32\Sgrm"
+rmdir /s /q "C:\Windows\Containers"
+rmdir /s /q "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\Modules\DefenderPerformance"
+rmdir /s /q "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Defender"
+rmdir /s /q "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\DefenderPerformance"
+rmdir /s /q "C:\Windows\System32\Tasks_Migrated\Microsoft\Windows\Windows Defender"
+rmdir /s /q "C:\Windows\System32\Tasks\Microsoft\Windows\Windows Defender"
+rmdir /s /q "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\Modules\Defender"
+rmdir /s /q "C:\Windows\System32\HealthAttestationClient"
+rmdir /s /q "C:\Windows\GameBarPresenceWriter"
+rmdir /s /q "C:\Windows\bcastdvr"
+del /f "C:\Windows\System32\SecurityAndMaintenance_Error.png"
+del /f "C:\Windows\System32\SecurityAndMaintenance.png"
+del /f "C:\Windows\System32\SecurityHealthSystray.exe"
+del /f "C:\Windows\System32\SecurityHealthService.exe"
+del /f "C:\Windows\System32\SecurityHealthHost.exe"
+del /f "C:\Windows\System32\drivers\SgrmAgent.sys"
+del /f "C:\Windows\System32\drivers\WdDevFlt.sys"
+del /f "C:\Windows\System32\drivers\WdBoot.sys"
+del /f "C:\Windows\System32\drivers\WdFilter.sys"
+del /f "C:\Windows\System32\wscsvc.dll"
+del /f "C:\Windows\System32\drivers\WdNisDrv.sys"
+del /f "C:\Windows\System32\wscsvc.dll"
+del /f "C:\Windows\System32\wscproxystub.dll"
+del /f "C:\Windows\System32\wscisvif.dll"
+del /f "C:\Windows\System32\SecurityHealthProxyStub.dll"
+del /f "C:\Windows\System32\smartscreen.dll"
+del /f "C:\Windows\SysWOW64\smartscreen.dll"
+del /f "C:\Windows\System32\smartscreen.exe"
+del /f "C:\Windows\SysWOW64\smartscreen.exe"
+del /f "C:\Windows\System32\DWWIN.EXE"
+del /f "C:\Windows\SysWOW64\smartscreenps.dll"
+del /f "C:\Windows\System32\smartscreenps.dll"
+del /f "C:\Windows\System32\SecurityHealthCore.dll"
+del /f "C:\Windows\System32\SecurityHealthSsoUdk.dll"
+del /f "C:\Windows\System32\SecurityHealthUdk.dll"
+del /f "C:\Windows\System32\SecurityHealthAgent.dll"
+del /f "C:\Windows\System32\wscapi.dll"
+del /f "C:\Windows\System32\wscadminui.exe"
+del /f "C:\Windows\SysWOW64\GameBarPresenceWriter.exe"
+del /f "C:\Windows\System32\GameBarPresenceWriter.exe"
+del /f "C:\Windows\SysWOW64\DeviceCensus.exe"
+del /f "C:\Windows\SysWOW64\CompatTelRunner.exe"
+del /f "C:\Windows\SysWOW64\mcupdate_AuthenticAMD.dll"
+del /f "C:\Windows\SysWOW64\mcupdate_GenuineIntel.dll"
+del /f "C:\Windows\System32\mcupdate_AuthenticAMD.dll"
+del /f "C:\Windows\System32\mcupdate_GenuineIntel.dll"
+del /f "C:\Windows\system32\drivers\msseccore.sys"
 :: Removing Services
 sc delete SgrmBroker
 sc delete SgrmAgent
