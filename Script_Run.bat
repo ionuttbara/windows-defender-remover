75902829dd588e14952fc18b37302f2c7b92e960
Ionut Bara <ionutbaraooo@gmail.com>
2023-11-22 00:19:14 +0200

    fixes

--- Script_Run.bat
+++ Script_Run.bat
@@ -1,5 +1,4 @@
 @echo off & pushd "%CD%" & CD /D "%~dp0"
-
 :--------------------------------------
 :: Arguments Section
 IF "%1"== NUL GOTO :menu
@@ -18,7 +17,7 @@ IF "%1"== "/r" GOTO :enabledef
 :--------------------------------------
 :menu
 cls
-echo ------Defender Remover Script , version 12.5 ------
+echo ------Defender Remover Script , version 12.6------
 echo Select an option:
 echo.
 echo Press (Y) for removing Defender and Security Components (old method, breaking Windows Updates/UWP in some version of Windows, removes files and unregisters classes)
@@ -43,17 +42,17 @@ goto :eof
 :removedef
 cls
 echo Killing Tasks...
-for /f "delims=" %%i in (Remover\TKL.txt) do (taskkill.exe /f /im "%%i") >nul
+for /f "delims=" %%i in (Remover\TKL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\taskkill.exe /f /im "%%i"") >nul
 cls
 echo Removing Windows Security UWP...
-for /d %%f in ("C:\Program Files\WindowsApps\Microsoft.SecHealthUI*") do (PowerRun.exe cmd.exe /c rmdir /s /q "%%f") >nul
+for /d %%f in ("C:\Program Files\WindowsApps\Microsoft.SecHealthUI*") do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /c rmdir /s /q "%%f"") >nul
 cls
 echo Applying Registry Files...
-for /r %%k in (Remover\REGS\*.reg) do (PowerRun.exe regedit.exe /s "%%k") >nul
+for /r %%k in (Remover\REGS\*.reg) do (GetTrustedInstaller.exe "C:\Windows\regedit.exe /s "%%k"") >nul
 cls
 echo Removing Windows Defender/Security Components Files...
-for /f "delims=" %%i in (Remover\FDL.txt) do (PowerRun.exe cmd.exe /c del /f /q "%%i") 1>nul
-for /f "delims=" %%i in (Remover\DDL.txt) do (PowerRun.exe cmd.exe /c rmdir /s /q "%%i") >nul
+for /f "delims=" %%i in (Remover\FDL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /c del /f /q "%%i"") >nul
+for /f "delims=" %%i in (Remover\DDL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /c rmdir /s /q "%%i"") >nul
 timeout /t 5 /nobreak
 shutdown /r /f /t 0
 goto :eof
@@ -71,15 +70,15 @@ goto :eof
 :tweaksdef
 CLS & echo Disable Defender and Security Components...
 :: Disable Defender's Scheduled Tasks
-PowerRun cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
-PowerRun.exe regedit.exe /s "DisablerS\Disable.reg" >nul
-PowerRun cmd.exe /k "taskkill /f /im smartscreen.exe & taskkill /f /im MsMpEngCP.exe & taskkill /f /im MsMpEng.exe"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender" "C:\ProgramData\Microsoft\Windows Defender Ma"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender" "C:\Program Files\Windows Defender Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection" "C:\Program Files\Windows Defender Advanced Threat Protection Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender" "C:\Program Files (x86)\Windows Defender Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
+GetTrustedInstaller.exe cmd.exe  /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
+GetTrustedInstaller.exe regedit.exe /s "DisablerS\Disable.reg" >nul
+GetTrustedInstaller.exe cmd.exe  /c "taskkill /f /im smartscreen.exe & taskkill /f /im MsMpEngCP.exe & taskkill /f /im MsMpEng.exe"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender" "C:\ProgramData\Microsoft\Windows Defender Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender" "C:\Program Files\Windows Defender Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection" "C:\Program Files\Windows Defender Advanced Threat Protection Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender" "C:\Program Files (x86)\Windows Defender Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
 cls & echo Antivirus and Security Components Disabled. A reboot is needed!
 timeout /t 5 /nobreak
 shutdown /r /f /t 0
@@ -90,14 +89,14 @@ goto :eof
 :enabledef
 CLS & echo Enable Defender and Security Components...
 :: Enable Defender's Scheduled Tasks
-PowerRun cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
-PowerRun.exe regedit.exe /s "DisablerS\Enable.reg"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Ma" "C:\ProgramData\Microsoft\Windows Defender"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Ma" "C:\Program Files\Windows Defender"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection Ma" "C:\Program Files\Windows Defender Advanced Threat Protection"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender Ma" "C:\Program Files (x86)\Windows Defender"
-PowerRun.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
+GetTrustedInstaller.exe cmd.exe  /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
+GetTrustedInstaller.exe regedit.exe /s "DisablerS\Enable.reg"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Ma" "C:\ProgramData\Microsoft\Windows Defender"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Ma" "C:\Program Files\Windows Defender"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection Ma" "C:\Program Files\Windows Defender Advanced Threat Protection"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender Ma" "C:\Program Files (x86)\Windows Defender"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
 cls & echo Antivirus and Windows Security Components Enabled. A reboot is needed!
 timeout /t 5 /nobreak
 shutdown /r /f /t 0
@@ -107,15 +106,15 @@ goto :eof
 :tweaksdefanti
 CLS & echo Disabling Defender...
 :: Disable Defender's Scheduled Tasks
-PowerRun cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
-PowerRun.exe regedit.exe /s "Disabler\Disable.reg" >nul
-PowerRun cmd.exe /k "taskkill /f /im smartscreen.exe & taskkill /f /im MsMpEngCP.exe & taskkill /f /im MsMpEng.exe"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender" "C:\ProgramData\Microsoft\Windows Defender Ma"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender" "C:\Program Files\Windows Defender Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection" "C:\Program Files\Windows Defender Advanced Threat Protection Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender" "C:\Program Files (x86)\Windows Defender Ma"
-PowerRun.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
+GetTrustedInstaller.exe cmd.exe  /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
+GetTrustedInstaller.exe regedit.exe /s "Disabler\Disable.reg" >nul
+GetTrustedInstaller.exe cmd.exe  /c "taskkill /f /im smartscreen.exe & taskkill /f /im MsMpEngCP.exe & taskkill /f /im MsMpEng.exe"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender" "C:\ProgramData\Microsoft\Windows Defender Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender" "C:\Program Files\Windows Defender Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection" "C:\Program Files\Windows Defender Advanced Threat Protection Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender" "C:\Program Files (x86)\Windows Defender Ma"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
 cls & echo Antivirus disabled. A reboot is needed!
 timeout /t 5 /nobreak
 shutdown /r /f /t 0
@@ -126,14 +125,14 @@ goto :eof
 :enabledefanti
 CLS & echo Enable Defender...
 :: Enable Defender's Scheduled Tasks
-PowerRun cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
-PowerRun.exe regedit.exe /s "Disabler\Enable.reg"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Ma" "C:\ProgramData\Microsoft\Windows Defender"
-PowerRun.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Ma" "C:\Program Files\Windows Defender"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection Ma" "C:\Program Files\Windows Defender Advanced Threat Protection"
-PowerRun.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender Ma" "C:\Program Files (x86)\Windows Defender"
-PowerRun.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
+GetTrustedInstaller.exe cmd.exe /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
+GetTrustedInstaller.exe regedit.exe /s "Disabler\Enable.reg"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Ma" "C:\ProgramData\Microsoft\Windows Defender"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Ma" "C:\Program Files\Windows Defender"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection Ma" "C:\Program Files\Windows Defender Advanced Threat Protection"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender Ma" "C:\Program Files (x86)\Windows Defender"
+GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
 cls & echo Antivirus Enabled. A reboot is needed!
 timeout /t 5 /nobreak
 shutdown /r /f /t 0
