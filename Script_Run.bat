741baf8738450d6a2aaa51fa953d0719793d9e5a
Ionut Bara <ionutbaraooo@gmail.com>
2023-11-22 18:39:30 +0200

    added condition of safe mode in safe methods

--- Script_Run.bat
+++ Script_Run.bat
@@ -45,98 +45,86 @@ echo Killing Tasks...
 for /f "delims=" %%i in (Remover\TKL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\taskkill.exe /f /im "%%i"") >nul
 cls
 echo Removing Windows Security UWP...
-for /d %%f in ("C:\Program Files\WindowsApps\Microsoft.SecHealthUI*") do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /c rmdir /s /q "%%f"") >nul
+for /d %%f in ("C:\Program Files\WindowsApps\Microsoft.SecHealthUI*") do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /k rmdir /s /q "%%f"") >nul
 cls
 echo Applying Registry Files...
 for /r %%k in (Remover\REGS\*.reg) do (GetTrustedInstaller.exe "C:\Windows\regedit.exe /s "%%k"") >nul
 cls
 echo Removing Windows Defender/Security Components Files...
-for /f "delims=" %%i in (Remover\FDL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /c del /f /q "%%i"") >nul
-for /f "delims=" %%i in (Remover\DDL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /c rmdir /s /q "%%i"") >nul
+for /f "delims=" %%i in (Remover\FDL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /k del /f /q "%%i"") >nul
+for /f "delims=" %%i in (Remover\DDL.txt) do (GetTrustedInstaller.exe "C:\Windows\System32\cmd.exe /k rmdir /s /q "%%i"") >nul
 timeout /t 5 /nobreak
 shutdown /r /f /t 0
-goto :eof
-:--------------------------------------
-
-
-:--------------------------------------
-:image
-start /wait ISOCreator.cmd
 goto :eof
 :--------------------------------------
 
 :--------------------------------------
 
 :tweaksdef
+if "%SAFEBOOT_OPTION%"=="" goto error
 CLS & echo Disable Defender and Security Components...
 :: Disable Defender's Scheduled Tasks
-GetTrustedInstaller.exe cmd.exe  /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
+GetTrustedInstaller.exe cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
 GetTrustedInstaller.exe regedit.exe /s "DisablerS\Disable.reg" >nul
-GetTrustedInstaller.exe cmd.exe  /c "taskkill /f /im smartscreen.exe & taskkill /f /im MsMpEngCP.exe & taskkill /f /im MsMpEng.exe"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender" "C:\ProgramData\Microsoft\Windows Defender Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender" "C:\Program Files\Windows Defender Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection" "C:\Program Files\Windows Defender Advanced Threat Protection Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender" "C:\Program Files (x86)\Windows Defender Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
+GetTrustedInstaller.exe cmd.exe /k move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
 cls & echo Antivirus and Security Components Disabled. A reboot is needed!
-timeout /t 5 /nobreak
-shutdown /r /f /t 0
+echo To exit from safe mode you must open an cmd and write this command and reboot.
+echo bcdedit /deletevalue {default} safeboot
+pause
 goto :eof
 :--------------------------------------
 
 :--------------------------------------
 :enabledef
+if "%SAFEBOOT_OPTION%"=="" goto error
 CLS & echo Enable Defender and Security Components...
 :: Enable Defender's Scheduled Tasks
-GetTrustedInstaller.exe cmd.exe  /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
+GetTrustedInstaller.exe cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
 GetTrustedInstaller.exe regedit.exe /s "DisablerS\Enable.reg"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Ma" "C:\ProgramData\Microsoft\Windows Defender"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Ma" "C:\Program Files\Windows Defender"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection Ma" "C:\Program Files\Windows Defender Advanced Threat Protection"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender Ma" "C:\Program Files (x86)\Windows Defender"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
+GetTrustedInstaller.exe cmd.exe /k move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
 cls & echo Antivirus and Windows Security Components Enabled. A reboot is needed!
-timeout /t 5 /nobreak
-shutdown /r /f /t 0
+echo To exit from safe mode you must open an cmd and write this command and reboot.
+echo bcdedit /deletevalue {default} safeboot
+pause
 goto :eof
 :--------------------------------------
 
+:--------------------------------------
 :tweaksdefanti
+if "%SAFEBOOT_OPTION%"=="" goto error
 CLS & echo Disabling Defender...
 :: Disable Defender's Scheduled Tasks
-GetTrustedInstaller.exe cmd.exe  /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
+GetTrustedInstaller.exe cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable"
 GetTrustedInstaller.exe regedit.exe /s "Disabler\Disable.reg" >nul
-GetTrustedInstaller.exe cmd.exe  /c "taskkill /f /im smartscreen.exe & taskkill /f /im MsMpEngCP.exe & taskkill /f /im MsMpEng.exe"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender" "C:\ProgramData\Microsoft\Windows Defender Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender" "C:\Program Files\Windows Defender Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection" "C:\Program Files\Windows Defender Advanced Threat Protection Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender" "C:\Program Files (x86)\Windows Defender Ma"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
+GetTrustedInstaller.exe cmd.exe /k move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\System32\smartscreen.plm"
 cls & echo Antivirus disabled. A reboot is needed!
-timeout /t 5 /nobreak
-shutdown /r /f /t 0
+echo To exit from safe mode you must open an cmd and write this command and reboot.
+echo bcdedit /deletevalue {default} safeboot
+pause
 goto :eof
 :--------------------------------------
 
 :--------------------------------------
 :enabledefanti
+if "%SAFEBOOT_OPTION%"=="" goto error
 CLS & echo Enable Defender...
 :: Enable Defender's Scheduled Tasks
-GetTrustedInstaller.exe cmd.exe /c "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
+GetTrustedInstaller.exe cmd.exe /k "schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Enable & schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Enable"
 GetTrustedInstaller.exe regedit.exe /s "Disabler\Enable.reg"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Ma" "C:\ProgramData\Microsoft\Windows Defender"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection Ma" "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Ma" "C:\Program Files\Windows Defender"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files\Windows Defender Advanced Threat Protection Ma" "C:\Program Files\Windows Defender Advanced Threat Protection"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Program Files (x86)\Windows Defender Ma" "C:\Program Files (x86)\Windows Defender"
-GetTrustedInstaller.exe cmd.exe /c move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
-cls & echo Antivirus Enabled. A reboot is needed!
-timeout /t 5 /nobreak
+GetTrustedInstaller.exe cmd.exe /k move /y "C:\Windows\System32\smartscreen.plm" "C:\Windows\System32\smartscreen.exe"
+echo To exit from safe mode you must open an cmd and write this command and reboot.
+echo bcdedit /deletevalue {default} safeboot
 shutdown /r /f /t 0
 :--------------------------------------
+
+:--------------------------------------
+:error
+echo To disable/enable Windows Defender you MUST to be in Safe Mode. Go to CMD and run this command and reboot.
+echo bcdedit /set {current} safeboot minimal
+pause
+exit
+:--------------------------------------
+
 
 :--------------------------------------
 :eof
