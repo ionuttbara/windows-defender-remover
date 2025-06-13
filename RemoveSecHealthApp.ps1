c5b3132b58b6a7dc056c9684b9d1fb88fa35de7e
sequencerr <45060278+sequencerr@users.noreply.github.com>
2025-06-13 14:13:58 +0200

    Update RemoveSecHealthApp.ps1

--- RemoveSecHealthApp.ps1
+++ RemoveSecHealthApp.ps1
@@ -2,10 +2,15 @@ $store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'
 $sids = @('S-1-5-18') # https://learn.microsoft.com/en-us/windows-server/identity/ad-ds/manage/understand-security-identifiers
 if (Test-Path $store) { $sids += Get-ChildItem $store -ea 0 | %{ $_.PSChildName } | ?{ $_.StartsWith('S-1-5-21') } }
 
-$appx = Get-AppxPackage -AllUsers -Name "Microsoft.Windows.SecHealthUI"
-if ($null -eq $appx) { return }
+if ((Get-CimInstance -Class Win32_OperatingSystem).Caption -Match "Windows 11") { 
+	$name = "Microsoft.SecHealthUI" 
+} else { 
+	$name = "Microsoft.Windows.SecHealthUI" 
+}
+$appx = Get-AppxPackage -AllUsers -Name $name
+if ($null -eq $appx) { return Write-Host "WindowsDefender Appx Package not found." -ForegroundColor Red }
 New-Item "$store\Deprovisioned\$($appx.PackageFamilyName)" -Force | Out-Null
 foreach ($sid in $sids) { New-Item "$store\EndOfLife\$sid\$($appx.PackageFullName)" -Force | Out-Null }
 
 DISM /Online /Set-NonRemovableAppPolicy /PackageFamily:$appx.PackageFamilyName /NonRemovable:0 | Out-Null
-Remove-AppxPackage -AllUsers -Package $appx.PackageFullName | Out-Null
+$appx | Remove-AppxPackage -AllUsers
